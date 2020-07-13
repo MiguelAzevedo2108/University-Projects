8bc840717cc092b1f846a77bb5cded175af8191a
Miguel Azevedo <miguelazevedo2108@gmail.com>
2020-07-13 18:18:15 +0000

    SO2

---
+++ BSc/2020-SO2-1Parte/trabalho1/src/trabalho1/Connect.java
@@ -0,0 +1,38 @@
+
+package trabalho1;
+
+import java.sql.*;
+
+public class Connect 
+{
+        java.sql.Connection con = null;
+        java.sql.Statement stmt = null;
+        
+        String PG_HOST = "localhost";
+        String PG_DB = "trabalho";
+        String USER = "l36975";
+        String PWD= "teste";
+    
+        public void connect()
+        {
+            try {
+                Class.forName("org.postgresql.Driver");
+
+                // url = "jdbc:postgresql://host:port/database",
+                con = DriverManager.getConnection("jdbc:postgresql://" + PG_HOST + ":5432/" + PG_DB,
+                        USER,
+                        PWD);
+
+                stmt = con.createStatement();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println("Problems setting the connection");
+            }
+        }
+        
+        public Statement getStatement()
+        {
+            return stmt;
+        }
+}
